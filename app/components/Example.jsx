05997d6080b763357c041f1c960b1e72456d32c6
Jan Viktor Adora <Jan Viktor Adora>
2017-02-14 18:46:12 +0800

    Updated Example.jsx

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -12,7 +12,7 @@ var React = require('react');
 var Example = (props) => {
   return (
     <div>
-      <h3>Example Component !</h3>
+      <h3>Example </h3>
       <p>Welcome to example page</p>
     </div>
   );
